cc936cf35b1c7385cd10cddc312f11fd6473f4bc
ZLH <test@example.com>
2026-01-01 23:05:10 +0800

    Fix JAVA_HOME error in clean.sh

--- clean.sh
+++ clean.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Set JAVA_HOME for Neo4j
+export JAVA_HOME=$(pwd)/tools/jdk-17.0.2
+export PATH=$JAVA_HOME/bin:$PATH
+
 # 1. Stop Neo4j
 ./tools/neo4j-community-4.4.34/bin/neo4j stop || true
 
